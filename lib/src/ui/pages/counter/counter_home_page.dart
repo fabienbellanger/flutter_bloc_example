5082302c2871704f5d71d90801398dc18a77b44a
Fabien Bellanger <valentil@gmail.com>
2019-05-11 23:28:02 +0200

    On tourne en rond

--- lib/src/ui/pages/counter/counter_home_page.dart
+++ lib/src/ui/pages/counter/counter_home_page.dart
@@ -12,38 +12,10 @@ class CounterHome extends StatefulWidget {
   _CounterHomeState createState() => _CounterHomeState();
 }
 
-class _CounterHomeState extends State<CounterHome> with SingleTickerProviderStateMixin {
-  AnimationController _animationController;
-  Animation<double> _animationSize;
-
-  @override
-  void initState() {
-    super.initState();
-
-    _animationController = new AnimationController(
-      duration: const Duration(milliseconds: 200),
-      vsync: this,
-    );
-
-    _animationController.addListener(() {
-      setState(() {});
-    });
-
-    _animationSize = new Tween(begin: 5.0, end: 64.0).animate(_animationController);
-
-    _animationController.forward();
-  }
-
-  @override
-  void dispose() {
-    _animationController.dispose();
-    super.dispose();
-  }
-
+class _CounterHomeState extends State<CounterHome> {
   @override
   Widget build(BuildContext context) {
     final IncrementBloc _incrementBloc = BlocProvider.of<IncrementBloc>(context);
-    double size = _animationSize?.value;
 
     return Center(
       child: StreamBuilder<int>(
@@ -53,14 +25,9 @@ class _CounterHomeState extends State<CounterHome> with SingleTickerProviderStat
             return Column(
               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: [
-                Text(
-                  snapshot.data.toString(),
-                  style: TextStyle(
-                    fontSize: size,
-                    color: Colors.blue,
-                  ),
+                TextCounter(
+                  counter: snapshot.data,
                 ),
-                Text('HOME'),
                 RaisedButton(
                   color: Colors.orange,
                   textColor: Colors.white,
@@ -83,6 +50,72 @@ class _CounterHomeState extends State<CounterHome> with SingleTickerProviderStat
               ],
             );
           }),
+    );
+  }
+}
+
+class TextCounter extends StatefulWidget {
+  final int counter;
+
+  const TextCounter({Key key, this.counter}) : super(key: key);
+
+  @override
+  _TextCounterState createState() => _TextCounterState();
+}
+
+class _TextCounterState extends State<TextCounter> with SingleTickerProviderStateMixin {
+  AnimationController _animationController;
+  Animation<double> _animationSize;
+  int counter;
+
+  @override
+  void initState() {
+    super.initState();
+
+    counter = widget.counter;
+
+    _animationController = new AnimationController(
+      duration: const Duration(milliseconds: 200),
+      vsync: this,
+    );
+
+    _animationController.addListener(() {
+      setState(() {});
+    });
+
+    _animationSize = new Tween(begin: 5.0, end: 64.0).animate(_animationController);
+
+    _animationController.forward();
+  }
+
+  @override
+  void didUpdateWidget(TextCounter oldWidget) {
+    if (counter != widget.counter) {
+      setState(() {
+        counter = widget.counter;
+
+        print('didUpdateWidget');
+      });
+    }
+    super.didUpdateWidget(oldWidget);
+  }
+
+  @override
+  void dispose() {
+    _animationController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    double size = _animationSize?.value;
+
+    return Text(
+      counter.toString(),
+      style: TextStyle(
+        fontSize: size,
+        color: Colors.blue,
+      ),
     );
   }
 }
