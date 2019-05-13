33028a767ee8dd1a50c498dd7a9d094154695b12
Fabien Bellanger <valentil@gmail.com>
2019-05-13 22:16:08 +0200

    Test solution

--- lib/main.dart
+++ lib/main.dart
@@ -24,3 +24,51 @@ class MyApp extends StatelessWidget {
     );
   }
 }
+
+class TestStreamBuilder extends StatefulWidget {
+  @override
+  _TestStreamBuilderState createState() => _TestStreamBuilderState();
+}
+
+class _TestStreamBuilderState extends State<TestStreamBuilder> with TickerProviderStateMixin {
+  Stream<int> stream;
+  AnimationController controller;
+  Animation colorAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+    stream = Stream.periodic(Duration(milliseconds: 1000), (i) => i).take(10);
+    controller = AnimationController(vsync: this, duration: Duration(milliseconds: 500));
+    colorAnimation = ColorTween(begin: Colors.white, end: Colors.orange).animate(controller);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox.expand(
+      child: Container(
+        child: FittedBox(
+          child: StreamBuilder(
+            stream: stream,
+            builder: (BuildContext context, AsyncSnapshot snapshot) {
+              if (snapshot.hasData) {
+                controller.forward(from: 0.0);
+                return AnimatedBuilder(
+                  animation: controller,
+                  builder: (BuildContext context, Widget child) {
+                    return Text(
+                      snapshot.data.toString(),
+                      style: TextStyle(color: colorAnimation.value),
+                    );
+                  },
+                );
+              }
+              return Container();
+            },
+          ),
+          fit: BoxFit.cover,
+        ),
+      ),
+    );
+  }
+}
--- lib/src/ui/pages/counter/counter_home_page.dart
+++ lib/src/ui/pages/counter/counter_home_page.dart
@@ -12,92 +12,23 @@ class CounterHome extends StatefulWidget {
   _CounterHomeState createState() => _CounterHomeState();
 }
 
-class _CounterHomeState extends State<CounterHome> {
-  @override
-  Widget build(BuildContext context) {
-    final IncrementBloc _incrementBloc = BlocProvider.of<IncrementBloc>(context);
-
-    return Center(
-      child: StreamBuilder<int>(
-          stream: _incrementBloc.outCounter,
-          initialData: _incrementBloc.counter,
-          builder: (BuildContext context, AsyncSnapshot<int> snapshot) {
-            return Column(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                TextCounter(
-                  counter: snapshot.data,
-                ),
-                RaisedButton(
-                  color: Colors.orange,
-                  textColor: Colors.white,
-                  disabledColor: Colors.grey,
-                  disabledTextColor: Colors.white70,
-                  padding: const EdgeInsets.all(10.0),
-                  child: const Text(
-                    'Go to TabBar Page',
-                    style: TextStyle(fontSize: 18.0),
-                  ),
-                  onPressed: () {
-                    Navigator.of(context).push(MaterialPageRoute(builder: (BuildContext context) {
-                      return BlocProvider<NavigationBloc>(
-                        bloc: NavigationBloc(),
-                        child: TabBarPage(),
-                      );
-                    }));
-                  },
-                ),
-              ],
-            );
-          }),
-    );
-  }
-}
-
-class TextCounter extends StatefulWidget {
-  final int counter;
-
-  const TextCounter({Key key, this.counter}) : super(key: key);
-
-  @override
-  _TextCounterState createState() => _TextCounterState();
-}
-
-class _TextCounterState extends State<TextCounter> with SingleTickerProviderStateMixin {
+class _CounterHomeState extends State<CounterHome> with SingleTickerProviderStateMixin {
   AnimationController _animationController;
   Animation<double> _animationSize;
-  int counter;
 
   @override
   void initState() {
     super.initState();
 
-    counter = widget.counter;
-
-    _animationController = new AnimationController(
+    _animationController = AnimationController(
       duration: const Duration(milliseconds: 200),
       vsync: this,
     );
-
     _animationController.addListener(() {
       setState(() {});
     });
-
-    _animationSize = new Tween(begin: 5.0, end: 64.0).animate(_animationController);
-
+    _animationSize = Tween(begin: 5.0, end: 64.0).animate(_animationController);
     _animationController.forward();
-  }
-
-  @override
-  void didUpdateWidget(TextCounter oldWidget) {
-    if (counter != widget.counter) {
-      setState(() {
-        counter = widget.counter;
-
-        print('didUpdateWidget');
-      });
-    }
-    super.didUpdateWidget(oldWidget);
   }
 
   @override
@@ -108,14 +39,54 @@ class _TextCounterState extends State<TextCounter> with SingleTickerProviderStat
 
   @override
   Widget build(BuildContext context) {
-    double size = _animationSize?.value;
+    final IncrementBloc _incrementBloc = BlocProvider.of<IncrementBloc>(context);
 
-    return Text(
-      counter.toString(),
-      style: TextStyle(
-        fontSize: size,
-        color: Colors.blue,
-      ),
+    return Center(
+      child: StreamBuilder<int>(
+          stream: _incrementBloc.outCounter,
+          initialData: _incrementBloc.counter,
+          builder: (BuildContext context, AsyncSnapshot<int> snapshot) {
+            double size = _animationSize?.value;
+            if (snapshot.hasData) {
+              return Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  AnimatedBuilder(
+                    animation: _animationController,
+                    builder: (BuildContext context, Widget child) {
+                      return Text(
+                        snapshot.data.toString(),
+                        style: TextStyle(
+                          fontSize: size,
+                          color: Colors.blue,
+                        ),
+                      );
+                    },
+                  ),
+                  RaisedButton(
+                    color: Colors.orange,
+                    textColor: Colors.white,
+                    disabledColor: Colors.grey,
+                    disabledTextColor: Colors.white70,
+                    padding: const EdgeInsets.all(10.0),
+                    child: const Text(
+                      'Go to TabBar Page',
+                      style: TextStyle(fontSize: 18.0),
+                    ),
+                    onPressed: () {
+                      Navigator.of(context).push(MaterialPageRoute(builder: (BuildContext context) {
+                        return BlocProvider<NavigationBloc>(
+                          bloc: NavigationBloc(),
+                          child: TabBarPage(),
+                        );
+                      }));
+                    },
+                  ),
+                ],
+              );
+            }
+            return Container();
+          }),
     );
   }
 }
